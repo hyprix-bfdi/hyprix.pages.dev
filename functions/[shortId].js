8efa93319f742a0d3053fb00b73770fce26c5418
hyprix-bfdi <absollutestore@gmail.com>
2025-01-11 22:40:55 +0800

    Update [shortId].js

--- functions/[shortId].js
+++ functions/[shortId].js
@@ -1,13 +1,21 @@
 export const onRequestGet = async ({ params, env }) => {
+  try {
     const { shortId } = params;
-  
+
+    if (!shortId) {
+      return new Response('Short ID is missing.', { status: 400 });
+    }
+
     // Retrieve the long URL from KV Storage
     const longUrl = await env.URLS.get(shortId);
-  
+
     if (longUrl) {
       return Response.redirect(longUrl, 301);
     } else {
-      return new Response('Short URL not found.', { status: 404 });
+      return new Response('Shortened URL not found.', { status: 404 });
     }
-  };
-  
+  } catch (err) {
+    console.error(err);
+    return new Response('Internal Server Error', { status: 500 });
+  }
+};
